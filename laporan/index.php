dbd5a3537be1dd0667032742d9aff5124b43cfd4
akhfa <akhmadfakhoni@gmail.com>
2015-02-10 14:21:06 +0700

    Isi laporan jadi

--- laporan/index.php
+++ laporan/index.php
@@ -1,38 +1,79 @@
 <?php
-	ini_set('display_errors', 1);
-	require '../database/config.php';
-	require ('cetak.php');
-	
-	$koneksi 	= getConnection();
-	//$query 		= "INSERT INTO Instansi VALUES ('namaa', 'emaill', 'passwordd')";
-	$query 		= "SELECT Judul, Lokasi, Nama, Tanggal, Instansi, Telepon, Status, Isi 
-					FROM Pengaduan LIMIT 5";
-	$result 	= getResultFromQuery($koneksi, $query);
-	//printResult($result,"Isi");
 
-	$pdf = new PDF();
-	$pdf->AliasNbPages();
-	//$pdf->Judul();
+	ini_set('display_errors', 1);
+function printLaporan($result)
+{
+
+	require_once('tcpdf/tcpdf.php');
+
+	$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+	// set document information
+	$pdf->SetCreator(PDF_CREATOR);
+	$pdf->SetAuthor('RePark');
+	$pdf->SetTitle('Laporan Pengaduan');
+
+	// set default header data
+	$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
+	$pdf->setFooterData(array(0,64,0), array(0,64,128));
+
+
+	// set header and footer fonts
+	$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+	$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+	// set default monospaced font
+	$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+	// set margins
+	$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+	$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+	$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+	// set auto page breaks
+	$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+	// set image scale factor
+	$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+	// set default font subsetting mode
+	$pdf->setFontSubsetting(true);
+
+	// Set font
+	// dejavusans is a UTF-8 Unicode font, if you only need to
+	// print standard ASCII chars, you can use core fonts like
+	// helvetica or times to reduce file size.
+	$pdf->SetFont('times', '', 12, '', true);
+
+
+
+	// Add a page
+	// This method has several options, check the source code documentation for more information.
 	$pdf->AddPage();
 
-	while($row = $result->fetch_assoc()) {
-		$pdf->SetFont('Times','B',12);
-		$pdf->CellFitScale(0,6,$row['Judul'],1,1);
+	// set text shadow effect
+	$pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
 
-		$pdf->SetFont('Times','',12);
-		$pdf->Cell(0,6,'Lokasi		: '.$row['Lokasi'],1,1);
-		$pdf->Cell(0,6,'Nama		: '.$row['Nama'],1,1);
-		$pdf->Cell(0,6,'Tanggal		: '.$row['Tanggal'],0,1);
-		$pdf->Cell(0,6,'Instansi	: '.$row['Instansi'],0,1);
-		$pdf->Cell(0,6,'Telepon		: '.$row['Telepon'],0,1);
-		$pdf->CellFitScale(0,6,'Isi 		: <br>'.$row['Isi'],1,1);
-		$pdf->Cell(0,6,'Status 		: '.$row['Status'],0,1);
-		$pdf->Ln();
-		$pdf->Ln();
+// Set some content to print
+	$html = '';
+	while($row = $result->fetch_assoc()) {
+		$judul 		= '<b>'.$row['Judul'].'</b><br>';
+		$lokasi 	= 'Lokasi 	: '.$row['Lokasi'].'<br>';
+		$nama 		= 'Nama 	: '.$row['Nama'].'<br>';
+		$tanggal 	= 'Tanggal 	: '.$row['Tanggal'].'<br>';
+		$instansi 	= 'Instansi : '.$row['Instansi'].'<br>';
+		$telepon 	= 'Telepon 	: '.$row['Telepon'].'<br>';
+		$isi 		= 'Isi 		: <br>'.$row['Isi'].'<br>';
+		$status 	= 'Status 	: '.$row['Status'];
+		$html = $html . $judul.$lokasi.$nama.$tanggal.$instansi.$telepon.$isi.$status.'<hr>'.'<br>'.'<br>';
 	}
 
-	
-	$pdf->Output();
-	
-	closeConnection($koneksi);
+	// Print text using writeHTMLCell()
+	$pdf->writeHTMLCell(0, 0, '', '', $html, 1, 1, 0, true, '', true);
+
+	$pdf->Output('Laporan.pdf', 'I');
+}
+
+//'judull', 'lokasii', 'namaa', 'tanggall', 'instansii', 'issii', 'statuss'
+//$judul, $lokasi, $nama, $tanggal, $instansi, $telepon, $isi, $status
 ?>
