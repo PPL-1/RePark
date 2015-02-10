e5103b45f0b7806d16990d70951fea88b4464a72
Tegar <tegarnization@gmail.com>
2015-02-10 14:44:17 +0700

    Homepage dibagusin dan bener

--- homepage/dev/index.php
+++ homepage/dev/index.php
@@ -1,140 +1,166 @@
-<?php
-	include 'header.php'
-?>
+<?php include 'header.php' ?>
 <!-- header -->
-	<div class="banner">
-		<div class="container">
-			<div class="header">
-				<div class="head-bann">
-						<div class="logo">
-							<a href="index.php"><img src="images/logo.png" alt=""></a>
-						</div>
-						<div class="head-part">
-								<ul>
-									<li><a href="#">Menu1 |</a></li>
-									<li><a href="#">Menu2 |</a></li>
-									<li><a href="contact.php">Menu3</a></li>
-									<li><!-- start search-->
-														<div class="search-box">
-															<div id="sb-search" class="sb-search">
-																<form>
-																	<input class="sb-search-input" placeholder="Tulis yang akan anda cari..." type="search" name="search" id="search">
-																	<input class="sb-search-submit" type="submit" value="">
-																	<span class="sb-icon-search"> </span>
-																</form>
-															</div>
-														</div>
-														<!-- search-scripts -->
-														<script src="js/classie.js"></script>
-														<script src="js/uisearch.js"></script>
-															<script>
-																new UISearch( document.getElementById( 'sb-search' ) );
-															</script>
-														<!-- //search-scripts -->
-									</li>
-								</ul>
-					</div>	
-					<div class="clearfix"> </div>
-						
-				</div>
-				<div class="head-nav">
-							<span class="menu"> </span>
-								<ul>
-									<li><a href="about.php">tentang kami</a></li>
-									<li class="active"><a href="index.php">beranda</a></li>
-									<li><a href="events.php">buat laporan</a></li>
-									<li><a href="shop.php">lacak laporan</a></li>
-									<li><a href="contact.php">cari laporan</a></li>
-									<div class="clearfix"> </div>
-							</ul>
-					</div>
-						<div class="clearfix"> </div>
-				
-					<!-- script-for-nav -->
-					<script>
-						$( "span.menu" ).click(function() {
-						  $( ".head-nav ul" ).slideToggle(300, function() {
-							// Animation complete.
-						  });
-						});
-					</script>
-				<!-- script-for-nav -->
-			</div> 
-		</div>
+<div class="banner">
+    <div class="container">
+        <div class="header">
+            <div class="head-bann">
+                <div class="logo">
+                    <a href="index.php"><img src="images/logo.png" alt="">
+                    </a>
+                </div>
+                <div class="titlelogo">
+                    <h3>Repark | Sistem Pengaduan Online Taman Bandung</h3>
+                </div>
+                <div class="head-part">
+                    <ul>
+                        <li><a href="#">Menu1 |</a>
+                        </li>
+                        <li><a href="#">Menu2 |</a>
+                        </li>
+                        <li><a href="contact.php">Menu3</a>
+                        </li>
+                        <li>
+                            <!-- start search-->
+                            <div class="search-box">
+                                <div id="sb-search" class="sb-search">
+                                    <form>
+                                        <input class="sb-search-input" placeholder="Tulis yang akan anda cari..." type="search" name="search" id="search">
+                                        <input class="sb-search-submit" type="submit" value="">
+                                        <span class="sb-icon-search"> </span>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- search-scripts -->
+                            <script src="js/classie.js"></script>
+                            <script src="js/uisearch.js"></script>
+                            <script>
+                            new UISearch(document.getElementById('sb-search'));
+                            </script>
+                            <!-- //search-scripts -->
+                        </li>
+                    </ul>
+                </div>
+                <div class="clearfix"> </div>
 
-			<div class="wmuSlider example1 section" id="section-1">
-			   <article style="position: absolute; width: 100%; opacity: 0;"> 
-			   	   	<div class="banner-info">
-						<h1>Selamat datang di Repark<span> Sistem Pelaporan Online Taman di Bandung</span></h1>
-					</div>
-				</article>
-				 <article style="position: absolute; width: 100%; opacity: 0;"> 
-			   	   	<div class="banner-info">
-						<h1>Selamat datang di Repark<span> Sistem Pelaporan Online Taman di Bandung</span></h1>
-					</div>
-				</article>
-				 <article style="position: absolute; width: 100%; opacity: 0;"> 
-			   	   		<div class="banner-info">
-						<h1>Selamat datang di Repark<span> Sistem Pelaporan Online Taman di Bandung</span></h1>
-					</div>
-				</article>
-				<ul class="wmuSliderPagination">
-                	<li><a href="#" class="">0</a></li>
-                	<li><a href="#" class="">1</a></li>
-                	<li><a href="#" class="">2</a></li>
+            </div>
+            <div class="head-nav">
+                <span class="menu"> </span>
+                <ul>
+                    <li><a href="tentangkami.php">tentang kami</a>
+                    </li>
+                    <li class="active"><a href="index.php">beranda</a>
+                    </li>
+                    <li><a href="kirimaduan.php">kirim aduan</a>
+                    </li>
+                    <li><a href="cariaduan.php">cari aduan</a>
+                    </li>
+                    <li><a href="daftartaman.php">daftar taman</a>
+                    </li>
+                    <div class="clearfix"> </div>
                 </ul>
-		  </div>		
-		
-		<!-- script -->
-          <script src="js/jquery.wmuSlider.js"></script> 
-			<script>
-       			$('.example1').wmuSlider();         
-   		    </script>
-			<!-- script -->		
-	</div>
+            </div>
+            <div class="clearfix"> </div>
+
+            <!-- script-for-nav -->
+            <script>
+            $("span.menu").click(function() {
+                $(".head-nav ul").slideToggle(300, function() {
+                    // Animation complete.
+                });
+            });
+            </script>
+            <!-- script-for-nav -->
+        </div>
+    </div>
+
+    <div class="wmuSlider example1 section" id="section-1">
+        <article style="position: absolute; width: 100%; opacity: 0;">
+            <div class="banner-info">
+                <h1>Selamat datang di Repark<span> Sistem Pelaporan Online Taman di Bandung</span></h1>
+            </div>
+        </article>
+        <article style="position: absolute; width: 100%; opacity: 0;">
+            <div class="banner-info">
+                <h1>Selamat datang di Repark<span> Sistem Pelaporan Online Taman di Bandung</span></h1>
+            </div>
+        </article>
+        <article style="position: absolute; width: 100%; opacity: 0;">
+            <div class="banner-info">
+                <h1>Selamat datang di Repark<span> Sistem Pelaporan Online Taman di Bandung</span></h1>
+            </div>
+        </article>
+        <ul class="wmuSliderPagination">
+            <li><a href="#" class="">0</a>
+            </li>
+            <li><a href="#" class="">1</a>
+            </li>
+            <li><a href="#" class="">2</a>
+            </li>
+        </ul>
+    </div>
+
+    <!-- script -->
+    <script src="js/jquery.wmuSlider.js"></script>
+    <script>
+    $('.example1').wmuSlider();
+    </script>
+    <!-- script -->
+</div>
 <!-- header -->
 
 <!-- content -->
 <div class="content">
-<div class="container">
-		<div class="row grids">
-			<div class="col-md-4 grid1">
-			  <a href="#">
-				<img src="images/img1.jpg" class="img-responsive" alt=""/>
-				<h3>mengadu</h3>
-				<div class="look">	
-					<ul>
-						<li><h4>Kirim aduan ke Pemerintah</h4></li>
-						<li><i class="arrow"></i></li>
-					</ul>
-				</div></a>
-			</div>
-			<div class="col-md-4 grid2">
-			  <a href="#">
-				<img src="images/img1.jpg" class="img-responsive" alt=""/>
-				<h3>melacak</h3>
-				<div class="look1">	
-					<ul>
-						<li><h4>Lacak aduan yang dikirimkan</h4></li>
-						<li><i class="arrow"></i></li>
-					</ul>
-				</div></a>
-			</div>
-			<div class="col-md-4 grid3">
-			  <a href="#">
-				<img src="images/img1.jpg" class="img-responsive" alt=""/>
-				<h3>mencari</h3>
-				<div class="look2">
-					<ul>
-						<li><h4>Cari aduan yang telah dikirim</h4></li>
-						<li><i class="arrow"></i></li>
-					</ul>
-				</div></a>
-			</div>
-				<div class="clearfix"> </div>
-		</div>
+    <div class="container">
+        <div class="row grids">
+            <div class="col-md-4 grid1" style="padding-left:75px">
+                <a href="#">
+                    <img src="images/img1.jpg" class="img-responsive" alt="" />
+                    <h3>mengadu</h3>
+                    <div class="look">
+                        <ul>
+                            <li>
+                                <h4>Kirim aduan ke Pemerintah</h4>
+                            </li>
+                            <li><i class="arrow"></i>
+                            </li>
+                        </ul>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 grid2" style="padding-left:75px">
+                <a href="#">
+                    <img src="images/img1.jpg" class="img-responsive" alt="" />
+                    <h3>mencari</h3>
+                    <div class="look1">
+                        <ul>
+                            <li>
+                                <h4>Cari aduan yang dikirimkan</h4>
+                            </li>
+                            <li><i class="arrow"></i>
+                            </li>
+                        </ul>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 grid3" style="padding-left:75px">
+                <a href="#">
+                    <img src="images/img1.jpg" class="img-responsive" alt="" />
+                    <h3>lihat taman</h3>
+                    <div class="look2">
+                        <ul>
+                            <li>
+                                <h4>Melihat taman di Bandung</h4>
+                            </li>
+                            <li><i class="arrow"></i>
+                            </li>
+                        </ul>
+                    </div>
+                </a>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+    </div>
 </div>
-</div>	
 <!-- content -->
 <!-- events -->
 <!-- 	<div class="events">
@@ -194,6 +220,4 @@
 		</div>
 	</div> -->
 <!-- events -->
-<?php
-	include 'footer.php'
-?>
+<?php include 'footer.php' ?>
