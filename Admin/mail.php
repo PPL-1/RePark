d05a0b63e2cb2b65edb26b3f4f36bfaaa56dc806
akhfa <akhmadfakhoni@gmail.com>
2015-02-10 15:56:53 +0700

    Insyaallah email fix. Tapi masih masuk spam

--- Admin/mail.php
+++ Admin/mail.php
@@ -4,10 +4,8 @@
 		$to 		= $alamat;
 		$subject 	= $judul;
 		$message	= $isi;
-		$from		= 'From: '.$namaPengirim.$asalEmail;
+		$from		= 'From: '.$namaPengirim.'<'.$asalEmail.'>';
 		mail($to, $subject, $message, $from);
 	}
 
-	sendMail('akhmadfakhoni@gmail.com', 'Laporan Taman Pintar', 'Pinter2 semuanya di sini', 'Akhmad Fakhoni', 'afld_kebumen@yahoo.co.id');
-	echo 'oke';
 ?>
