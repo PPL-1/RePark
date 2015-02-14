a95f549cd6f4e4e37ce8da1bed909dded0fd4b69
akhfa <akhmadfakhoni@gmail.com>
2015-02-14 08:48:34 +0700

    Bersih2 comment

--- laporan/cetak.php
+++ laporan/cetak.php
@@ -8,53 +8,31 @@ function printLaporan($result)
 
 	$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-	// set document information
-	$pdf->SetCreator(PDF_CREATOR);
-	$pdf->SetAuthor('RePark');
-	$pdf->SetTitle('Laporan Pengaduan');
-
-	// set default header data
 	$pdf->SetHeaderData('', 0, 'Dinas Pemakaman dan Pertamanan', 'Kota Bandung', array(0,64,255), array(0,64,128));
 	$pdf->setFooterData(array(0,64,0), array(0,64,128));
 
-
-	// set header and footer fonts
 	$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
 	$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 
-	// set default monospaced font
 	$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
-	// set margins
+	//Set Margin
 	$pdf->SetMargins(PDF_MARGIN_LEFT, 20, PDF_MARGIN_RIGHT);
 	$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
 	$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
-	// set auto page breaks
 	$pdf->SetAutoPageBreak(TRUE, 13);
 
-	// set image scale factor
 	$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
-	// set default font subsetting mode
 	$pdf->setFontSubsetting(true);
 
-	// Set font
-	// dejavusans is a UTF-8 Unicode font, if you only need to
-	// print standard ASCII chars, you can use core fonts like
-	// helvetica or times to reduce file size.
 	$pdf->SetFont('times', '', 12, '', true);
 
-
-
-	// Add a page
-	// This method has several options, check the source code documentation for more information.
 	$pdf->AddPage();
 
-	// set text shadow effect
 	$pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
 
-// Set some content to print
 	$html = '';
 	while($row = $result->fetch_assoc()) {
 		$judul 		= '<b>'.$row['Judul'].'</b><br>';
@@ -73,6 +51,4 @@ function printLaporan($result)
 	$pdf->Output('Laporan.pdf', 'I');
 }
 
-//'judull', 'lokasii', 'namaa', 'tanggall', 'instansii', 'issii', 'statuss'
-//$judul, $lokasi, $nama, $tanggal, $instansi, $telepon, $isi, $status
 ?>
