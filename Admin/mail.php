cd72ff16922c595551af0ea55c32024a3686c7a2
akhfa <akhmadfakhoni@gmail.com>
2015-02-15 05:52:17 +0700

    added testmail

--- Admin/mail.php
+++ Admin/mail.php
@@ -1,10 +1,11 @@
 <?php
-	function sendMail($alamat, $judul, $isi, $namaPengirim, $asalEmail)
+	ini_set('display_errors', 1);
+	function sendMail($alamat, $judul, $isi, $namaPengirim)
 	{
 		$to 		= $alamat;
 		$subject 	= $judul;
 		$message	= $isi;
-		$from		= 'From: '.$namaPengirim.'<'.$asalEmail.'>';
+		$from		= 'From: '.$namaPengirim.'<reparkyou@gmail.com>';
 		mail($to, $subject, $message, $from);
 	}
 
---
+++ Admin/testmail.php
@@ -0,0 +1,6 @@
+<?php
+	ini_set('display_errors', 1);
+	require 'mail.php';
+	sendMail('akhmadfakhoni@gmail.com', 'Taman oke', 'from local', 'akhfa');
+	echo 'email sent';
+?> 
