84696aa5ddc5a51adab0f2452db7192aac22ff52
akhfa <akhmadfakhoni@gmail.com>
2015-02-14 09:21:07 +0700

    add blank cover page

--- laporan/cetak.php
+++ laporan/cetak.php
@@ -8,31 +8,30 @@ function printLaporan($result)
 
 	$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
+	//Set Margin
+	$pdf->SetMargins(PDF_MARGIN_LEFT, 20, PDF_MARGIN_RIGHT);
+
+	$pdf->SetPrintHeader(false);
+	$pdf->SetPrintFooter(false);
+	$pdf->AddPage();
+
+	$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+	$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
 	$pdf->SetHeaderData('', 0, 'Dinas Pemakaman dan Pertamanan', 'Kota Bandung', array(0,64,255), array(0,64,128));
 	$pdf->setFooterData(array(0,64,0), array(0,64,128));
 
 	$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
 	$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 
-	$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-
-	//Set Margin
-	$pdf->SetMargins(PDF_MARGIN_LEFT, 20, PDF_MARGIN_RIGHT);
-	$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-	$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-
 	$pdf->SetAutoPageBreak(TRUE, 13);
-
-	$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-
-	$pdf->setFontSubsetting(true);
 
 	$pdf->SetFont('times', '', 12, '', true);
 
+	$pdf->SetPrintHeader();
+	$pdf->startPageGroup();
 	$pdf->AddPage();
-
-	$pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
-
+	$pdf->SetPrintFooter();
 	$html = '';
 	while($row = $result->fetch_assoc()) {
 		$judul 		= '<b>'.$row['Judul'].'</b><br>';
